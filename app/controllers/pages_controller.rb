1399ac5040d73bb9df2f6cf67f11e94d7d7bcf4b
Marielle Louise Cruz <mariellecruz@yahoo.com>
2018-11-13 13:15:25 +0000

    Added custom Google fonts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
-    def home
+    def home # The FUNCTION, home. Though when you have functions inside of a class, you call it METHOD
+    # The file name 'home' (from VIEWS) needs to match the METHOD name 'home' in the CONTROLLER
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#home' # Root is like a HOME PAGE; HOME ACTION/METHOD
+  # This ROUTES file is the 1st file that gets activated
 end
