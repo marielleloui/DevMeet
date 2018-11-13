1120f203e17bfe8d16e7ffeabf3599e31a43640b
Marielle Louise Cruz <mariellecruz@yahoo.com>
2018-11-13 13:43:21 +0000

    Added an about view, controller action, and route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
     def home # The FUNCTION, home. Though when you have functions inside of a class, you call it METHOD
     # The file name 'home' (from VIEWS) needs to match the METHOD name 'home' in the CONTROLLER
     end
+    
+    def about
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home' # Root is like a HOME PAGE; HOME ACTION/METHOD
   # This ROUTES file is the 1st file that gets activated
+  get  'about', to: 'pages#about' # pages#about refers to the About METHOD in PAGES CONTROLLER
 end
