2b942ee5079b368e073d1f248ccd8e285470dc5c
Marielle Louise Cruz <mariellecruz@yahoo.com>
2018-11-15 23:52:28 +0000

    Added custom Contact Us path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # This ROUTES file is the 1st file that gets activated
   get  'about', to: 'pages#about' # pages#about refers to the About METHOD in PAGES CONTROLLER
   resources :contacts
+  get 'contact-us', to: 'contacts#new'
 end
