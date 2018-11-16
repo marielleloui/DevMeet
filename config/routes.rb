b35218d093befc6ac1aaded6632aa9163cc20cbc
Marielle Louise Cruz <mariellecruz@yahoo.com>
2018-11-16 01:12:39 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home' # Root is like a HOME PAGE; HOME ACTION/METHOD
   # This ROUTES file is the 1st file that gets activated
   get  'about', to: 'pages#about' # pages#about refers to the About METHOD in PAGES CONTROLLER
-  resources :contacts
-  get 'contact-us', to: 'contacts#new'
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
