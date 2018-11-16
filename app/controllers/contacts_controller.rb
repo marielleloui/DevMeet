2b4fdb9243fae0f7a5edfafb50fdea0542b344f8
Marielle Louise Cruz <mariellecruz@yahoo.com>
2018-11-16 02:16:21 +0000

    Triggered the mailer from the Contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,13 @@ class ContactsController < ApplicationController
     end
     
     def create
-        @contact = Contact.new(contact_params)
+        @contact = Contact.new(contact_params) # params is kind of like mass assignment; refers to things we are passing through form fields or links; contact_params is a function that we previously defined and is found below
         if @contact.save
+            # This is how we lift data from form fields here in the controller
+            name = params[:contact][:name] # Assigning a var name to params
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, body).deliver # Calling function contact_email from contact_mailer "CONTROLLER" and using the vars above as arguments. The .deliver method will fire off the email to the recipient
             flash[:success] = "Message sent."
             redirect_to new_contact_path
         else
