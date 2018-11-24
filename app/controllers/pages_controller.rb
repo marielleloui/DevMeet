fac98b3c9c9854f92e829db9ed2ceb4dd41f469e
Marielle Cruz <mariellecruz@yahoo.com>
2018-11-24 08:00:57 +0000

    Added plan variables and parameters to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
+    
+    # GET request for / which is the home page
     def home # The FUNCTION, home. Though when you have functions inside of a class, you call it METHOD
     # The file name 'home' (from VIEWS) needs to match the METHOD name 'home' in the CONTROLLER
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+   
     end
     
     def about
